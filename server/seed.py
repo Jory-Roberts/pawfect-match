62212ac0ac0b0c574fac4d955c6c5cb51b1bc828
Jory Roberts <robertsjory13@gmail.com>
2023-09-23 23:24:54 -0500

    changes dog seed images to url instead of storing locacally

--- server/seed.py
+++ server/seed.py
@@ -8,19 +8,84 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, User, Dog, Adoption, Visit
+from models import db, User, Dog, Adoption, Review, Visit
 
 
 def seed_dogs():
-    dog1 = Dog(
+    dogs = []
+
+    d1 = Dog(
         name="Cheyenne",
         breed="Alaskan Malamute",
         age=5,
         gender="Female",
-        description="Goodest Girl",
-        image_url="server/seed_images/Alaskanmalamute0b.jpg",
+        description="Goodest girl",
+        image_url="https://commons.wikimedia.org/wiki/List_of_dog_breeds#/media/File:Alaskanmalamute0b.jpg",
     )
-    db.session.add(dog1)
+    dogs.append(d1)
+
+    d2 = Dog(
+        name="Chroizo",
+        breed="Dachsund",
+        age=2,
+        gender="Male",
+        description="Short but sweet",
+        image_url="https://commons.wikimedia.org/wiki/List_of_dog_breeds#/media/File:MiniDachshund1_wb.jpg",
+    )
+
+    dogs.append(d2)
+
+    d3 = Dog(
+        name="Raja",
+        breed="Australian Shepherd",
+        age=3,
+        gender="Male",
+        description="Hope you like hair!",
+        image_url="https://commons.wikimedia.org/wiki/List_of_dog_breeds#/media/File:Australian_Shepherd.jpg",
+    )
+
+    dogs.append(d3)
+
+    d4 = Dog(
+        name="Fran",
+        breed="Pembroke Welsh Corgi",
+        age=4,
+        gender="Female",
+        description="Hi! I'm down here! I need attention!",
+        image_url="https://commons.wikimedia.org/wiki/List_of_dog_breeds#/media/File:Pembroke_Welsh_Corgi_600.jpg",
+    )
+
+    dogs.append(d4)
+
+    d5 = Dog(
+        name="Jake",
+        breed="Bernese mountain dog",
+        age=9,
+        gender="Male",
+        description="Prefer cuddles",
+        image_url="https://commons.wikimedia.org/wiki/List_of_dog_breeds#/media/File:Berne%C5%84ski_pies_pasterski.jpg",
+    )
+
+    dogs.append(d5)
+
+    db.session.add_all(dogs)
+    db.session.commit()
+
+
+def seed_users():
+    pass
+
+
+def seed_adoptions():
+    pass
+
+
+def seed_reviews():
+    pass
+
+
+def seed_visits():
+    pass
 
 
 def main():
